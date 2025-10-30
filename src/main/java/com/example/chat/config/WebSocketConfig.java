e01981b2c8243648768c548f64516dee0dbc8be6
muniebazia <munieba.zia@mail.utoronto.ca>
2025-10-30 00:01:40 -0400

    Implementation of WebSocketConfig.java

--- src/main/java/com/example/chat/config/WebSocketConfig.java
+++ src/main/java/com/example/chat/config/WebSocketConfig.java
@@ -1,4 +1,25 @@
 package com.example.chat.config;
 
-public class WebSocketConfig {
+import com.example.chat.ws.ChatWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final ChatWebSocketHandler handler;
+
+    public WebSocketConfig(ChatWebSocketHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry r) {
+        WebSocketHandler handler;
+        r.addHandler(handler, "/ws").setAllowedOrigins("*");
+    }
 }
+
