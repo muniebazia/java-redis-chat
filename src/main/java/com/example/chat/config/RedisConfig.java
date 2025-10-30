f8217e02a6c4bbf1f58c35654dda7a5f6af5ffbd
muniebazia <munieba.zia@mail.utoronto.ca>
2025-10-30 19:26:10 -0400

    Changes to configuration classes

--- src/main/java/com/example/chat/config/RedisConfig.java
+++ src/main/java/com/example/chat/config/RedisConfig.java
@@ -1,4 +1,24 @@
 package com.example.chat.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
 public class RedisConfig {
+
+    // connection
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    // used in ChatWebSocketHandler.java
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        return new StringRedisTemplate(factory);
+    }
+
 }
--- src/main/java/com/example/chat/config/WebSocketConfig.java
+++ src/main/java/com/example/chat/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.example.chat.config;
 
 import com.example.chat.ws.ChatWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -13,12 +12,12 @@ public class WebSocketConfig implements WebSocketConfigurer {
     private final ChatWebSocketHandler handler;
 
     public WebSocketConfig(ChatWebSocketHandler handler) {
+
         this.handler = handler;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry r) {
-        WebSocketHandler handler;
         r.addHandler(handler, "/ws").setAllowedOrigins("*");
     }
 }
